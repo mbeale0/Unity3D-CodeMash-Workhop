fccd16a4a929e0aeaad5019cdcca5f39142e82b0
Mason Beale <mbeale@cedarville.edu>
2023-08-19 19:33:54 -0400

    Finished RPS Game

--- RockPaperScissors/Assets/Scripts/GameController.cs
+++ RockPaperScissors/Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,31 +6,102 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private int[] images = null;
+    [SerializeField] private Sprite[] images = null;
     [SerializeField] private GameObject playerImageObject = null;
     [SerializeField] private GameObject cpuImageObject = null;
-    private int count = 0;
+    private Dictionary<int, Choice> choices = new Dictionary<int, Choice>();
+    private Dictionary<Choice, Choice> whatLosesAgainst = new Dictionary<Choice, Choice>();
+    private Choice currChoice; 
     private int currImageIndex = 0;
-    public void OnCount()
+    private int cpuChoiceIndex = 0;
+    private bool isPlaying = false;
+    private void Start()
     {
-        count++;
-        Debug.Log($"Count: {count}");
+        playerImageObject.GetComponent<Image>().sprite = images[currImageIndex];
+        currChoice = currChoice = Choice.Paper;
+        choices.Add(0, Choice.Paper);
+        choices.Add(1, Choice.Rock);
+        choices.Add(2, Choice.Scissors);
+
+        whatLosesAgainst.Add(Choice.Rock, Choice.Scissors);
+        whatLosesAgainst.Add(Choice.Paper, Choice.Rock);
+        whatLosesAgainst.Add(Choice.Scissors, Choice.Paper);
     }
 
-    public void OnSelectionUp(){
-        currImageIndex--
+    public void OnSelectionLeft(){
+        currImageIndex--;
         if(currImageIndex < 0){
-            currImageIndex = images.length - 1;
+            currImageIndex = images.Length - 1;
         }
-        playerImageObject.GetComponent<Image>() = images[currImageIndex];
+        SetImageAndChoice();
     }
 
-    public void OnSelectionDown(){
+    public void OnSelectionRight()
+    {
         currImageIndex++;
-        if(currImageIndex == currImageIndex.index){
+        if (currImageIndex == images.Length)
+        {
             currImageIndex = 0;
         }
-        playerImageObject.GetComponent<Image>() = images[currImageIndex];
+        SetImageAndChoice();
     }
-    
+
+    public void OnPlay()
+    {
+        if(!isPlaying)
+        {
+            isPlaying = true;
+            Choice cpuChoice = GetCPUChoice();
+            StartCoroutine(CycleCPUChoices(CheckResults, cpuChoice));
+        }
+    }
+
+    private IEnumerator CycleCPUChoices(Action<Choice> callback, Choice cpuChoice)
+    {
+        IncrementCPUChoice();
+        yield return new WaitForSeconds(.5f);
+        IncrementCPUChoice();
+        yield return new WaitForSeconds(.5f);
+        IncrementCPUChoice();
+        yield return new WaitForSeconds(.5f);
+        callback(cpuChoice);
+    }
+
+    private void IncrementCPUChoice()
+    {
+        cpuChoiceIndex++;
+        if (cpuChoiceIndex == images.Length) cpuChoiceIndex = 0;
+        cpuImageObject.GetComponent<Image>().sprite = images[cpuChoiceIndex];
+    }
+
+    private Choice GetCPUChoice()
+    {
+        cpuChoiceIndex = UnityEngine.Random.Range(0, choices.Count);
+        cpuImageObject.GetComponent<Image>().sprite = images[cpuChoiceIndex];
+        Choice cpuChoice = choices[cpuChoiceIndex];
+        return cpuChoice;
+    }
+
+    private void CheckResults(Choice cpuChoice)
+    {
+        if (cpuChoice == currChoice)
+        {
+            Debug.Log("Tied");
+        }
+        else if (cpuChoice == whatLosesAgainst[currChoice])
+        {
+            Debug.Log("You won!");
+        }
+        else
+        {
+            Debug.Log("You lost");
+        }
+        isPlaying = false;
+    }
+
+    private void SetImageAndChoice()
+    {
+        currChoice = choices[currImageIndex];
+        playerImageObject.GetComponent<Image>().sprite = images[currImageIndex];
+    }
 }
