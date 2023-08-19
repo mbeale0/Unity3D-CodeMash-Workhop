a30063a8bff6b0ee963c8f8dca1560555da71502
MasonBeale <mbeale@cedarville.edu>
2023-08-19 17:11:45 -0400

    Started adding controls for images

--- RockPaperScissors/Assets/Scripts/GameController.cs
+++ RockPaperScissors/Assets/Scripts/GameController.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    int count = 0;
+    [SerializeField] private int[] images = null;
+    [SerializeField] private GameObject playerImageObject = null;
+    [SerializeField] private GameObject cpuImageObject = null;
+    private int count = 0;
+    private int currImageIndex = 0;
     public void OnCount()
     {
         count++;
         Debug.Log($"Count: {count}");
     }
+
+    public void OnSelectionUp(){
+        currImageIndex--
+        if(currImageIndex < 0){
+            currImageIndex = images.length - 1;
+        }
+        playerImageObject.GetComponent<Image>() = images[currImageIndex];
+    }
+
+    public void OnSelectionDown(){
+        currImageIndex++;
+        if(currImageIndex == currImageIndex.index){
+            currImageIndex = 0;
+        }
+        playerImageObject.GetComponent<Image>() = images[currImageIndex];
+    }
+    
 }
