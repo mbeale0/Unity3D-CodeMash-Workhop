5aefab168885873a8c20dd56548696f7b9eedbb4
Mason Beale <mbeale@cedarville.edu>
2023-11-17 17:06:53 -0500

    Lab 7 is Finished

--- RollerBall/Assets/Scripts/GameManager.cs
+++ RollerBall/Assets/Scripts/GameManager.cs
@@ -9,13 +9,20 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text gameOverText = null;
     [SerializeField] private TMP_Text coinText = null;
     [SerializeField] private GameObject pauseCanvas = null;
+    [SerializeField] private TMP_Text livesText = null;
+    [SerializeField] private Transform start = null;
+    [SerializeField] private Transform player = null;
 
     private int numberOfCoins = 0;
+    private int lives = 1;
+    private Vector3 checkpoint = Vector3.zero;
+
     void Start()
     {
         gameOverCanvas.SetActive(false);
         pauseCanvas.SetActive(false);
         coinText.text = $"Coins: {numberOfCoins}";
+        livesText.text = $"Lives: {lives}";
     }
 
     public void HandleGameOver(string textToDisplay)
@@ -35,6 +42,29 @@ public class GameManager : MonoBehaviour
     {
         numberOfCoins++;
         coinText.text = $"Coins: {numberOfCoins}";
+        if(numberOfCoins % 2 == 0)
+        {
+            lives++;
+            livesText.text = $"Lives: {lives}";
+        }
+    }
+
+    public void SetCurrentCheckpoint(Vector3 position)
+    {
+        checkpoint = position;
+    }
+    public void Respawn()
+    {
+        lives--;
+        livesText.text = $"Lives: {lives}";
+        if (lives < 0)
+        {
+            HandleGameOver("You lose!");
+        }
+        else
+        {
+            player.position = new Vector3(checkpoint.x, checkpoint.y + 5, checkpoint.z);
+        }
     }
 
 }
